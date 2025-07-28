f002f825580b11662de6fb6e284eb0c541f7f261
Paparazi灬 <2639739215@qq.com>
2025-07-28 19:17:15 +0800

    cut down building libs

--- cmake/pluggables_defines.cmake
+++ cmake/pluggables_defines.cmake
@@ -1,7 +1,6 @@
 # define pluggable implementations library building names in this file
 
 # multi versions lib building names
-set(SEQ_BASED_MULTI_VERSIONS_SHARED_LIB seq_based_multi_versions-shared)
 set(MULTI_VERSIONS_SHARED_LIB multi_versions-shared)
 
 # txn lock manager lib building names
@@ -14,7 +13,6 @@ set(STAGING_WRITE_SHARED_LIB staging_write-shared)
 set(MVCC_WRITE_BUFFER_SHARED_LIB mvcc_write_buffer-shared)
 
 # transaction lib building names
-set(MVCC_TRANSACTION_SHARED_LIB mvcc_transaction-shared)
 set(TRANSACTION_SHARED_LIB transaction-shared)
 
 # txn store lib building names
